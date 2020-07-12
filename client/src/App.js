37a38d01b33cb38a7db45c083c28e8b9bc9624a3
roee030 <roee.angel93@gmail.com>
2020-07-12 18:41:53 +0300

    add task option
    todo: table

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Navbar from './components/navbar/Navbar.js';
 import Title from './components/title/Title.js';
 import SearchBar from './components/searchBar/SearchBar.js';
+import TaskOption from './components/taskOptions/TaskOption';
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
         <div className='container'>
             <Title/>
             <SearchBar/>
+            <TaskOption/>
         </div>
     </>
   );
---
+++ client/src/components/taskOptions/TaskOption.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TaskOption.css';
+export default function TaskOption() {
+    return (
+        <div className='wrapper'>
+            <p className="listOfCostumer">רשימת הלקוחות שלך</p>
+            <button className='newTask'>משימה חדשה</button>
+        </div>
+    )
+}
