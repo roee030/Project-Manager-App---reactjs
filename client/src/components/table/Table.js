fbc87dcebe23e673de208baea80e6d60b9c14835
roee030 <roee.angel93@gmail.com>
2020-07-13 17:37:22 +0300

    fetch data from MYSQL to the table

--- client/src/components/table/Table.js
+++ client/src/components/table/Table.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import './Table.css'
+import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -15,44 +16,53 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+// function createData(name, calories, fat, carbs, protein) {
+//   return { name, calories, fat, carbs, protein };
+// }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-];
+// const rows = [
+//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//   createData('Eclair', 262, 16.0, 24),
+// ];
 
 export default function AcccessibleTable() {
-  const classes = useStyles();
-
-  return (
-    <TableContainer className="tablefff" component={Paper}>
+    const classes = useStyles();
+    const [data,setData] = useState([]);
+    useEffect(()=>{
+        axios
+        .get(`http://localhost:5000/getall`)
+        .then(res => {
+            console.log(res.data.data)
+            setData(res.data.data)
+        })
+        .catch(err =>{
+            console.log(err)
+        })
+        },[])
+   
+    return (
+    <TableContainer dir='rtl' className="tablefff" component={Paper}>
       <Table className={classes.table} aria-label="caption table">
-        <caption>A basic table example with a caption</caption>
         <TableHead>
           <TableRow className="rowBar">
-            <TableCell align="right">פעולות</TableCell>
-            <TableCell align="right">תאריך יצירת המשימה</TableCell>
-            <TableCell align="right">מייל</TableCell>
-            <TableCell align="right">טלפון</TableCell>
-            <TableCell align="right">שם משתמש</TableCell>
+          <TableCell align="right">שם משתמש</TableCell>
+          <TableCell align="right">טלפון</TableCell>
+          <TableCell align="right">מייל</TableCell>
+          <TableCell align="right">תאריך יצירת המשימה</TableCell>
+          <TableCell align="right">פעולות</TableCell>
           </TableRow>
         </TableHead>
         <TableBody >
-          {rows.map((row) => (
-            <TableRow key={row.name} className ='rows'>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+          {data ? data.map((row) => (
+            <TableRow key={row.User_Name} className ='rows'>
+              <TableCell align="right" component="th" scope="row">{row.User_Name}</TableCell>
+              <TableCell align="right">{row.Phone_Number}</TableCell>
+              <TableCell align="right">{row.Mail}</TableCell>
+              <TableCell align="right">{row.Date}</TableCell>
+              <TableCell align="right"></TableCell>
             </TableRow>
-          ))}
+          )) : <div>Loading</div>}
         </TableBody>
       </Table>
     </TableContainer>
